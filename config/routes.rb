033d1ee5c84e4757fa75988fe46de44136350011
Simon <simon@simonhamilton.ca>
2018-03-01 01:55:45 +0000

    added user authentication, log-in, register, logout and sessions with password encryption

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,15 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  #login form / login / logout
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # User signup form and create user in database
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
